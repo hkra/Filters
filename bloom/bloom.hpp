1bb56e5d315b99b1f16e7a685a74d5c7235d02d9
Dan <dan@imagine-orb.com>
2017-06-30 15:06:48 +0900

    Add program options and switch stdlib bitset for boost dynamic_bitset

--- bloom/bloom.hpp
+++ bloom/bloom.hpp
@@ -2,7 +2,8 @@
 
 #include <bitset>
 #include <vector>
-#include <city.h>
+#include <string>
+#include <boost/dynamic_bitset.hpp>
 
 typedef uint64 (*HashFn)(const char *s, size_t len, uint64 seed);
 
@@ -11,17 +12,32 @@ struct HashPair {
     uint64 seed;
 };
 
-template<typename T>
 class BloomFilter 
 {
 public:
 
-    explicit BloomFilter(const std::vector<HashPair> & hashSet) :
-        hashSet(hashSet) {
+    explicit BloomFilter(size_t size, const std::vector<HashPair> & hashSet) :
+        hashSet(hashSet), data(size) {
+    }
+
+    void insert(const std::string & item) {
+        for (auto it = hashSet.begin(); it != hashSet.end(); ++it) {
+            auto index = (*it).fn(item.c_str(), item.size(), (*it).seed) % data.size();
+            data.set(index);
+        }
+    }
+
+    bool exists(const std::string & item) const {
+        for (auto it = hashSet.begin(); it != hashSet.end(); ++it) {
+            auto index = (*it).fn(item.c_str(), item.size(), (*it).seed) % data.size();
+            if (!data[index])
+                return false;
+        }
+        return true;
     }
 
 private:
 
-    std::bitset<512> data;
+    boost::dynamic_bitset<> data;
     const std::vector<HashPair> & hashSet;
 };
--- bloom/driver.cpp
+++ bloom/driver.cpp
@@ -1,18 +1,74 @@
+#include <iostream>
+#include <fstream>
 #include <vector>
 #include <string>
+#include <boost/program_options.hpp>
+#include <city.h>
+
 #include "bloom.hpp"
+
+using namespace std;
+namespace po = boost::program_options;
+
+const size_t DEFAULT_FILTER_SIZE = 1 << 16;
+
+const vector<HashPair> DEFAULT_HASH_SET {
+    { CityHash64WithSeed, 0xDEADBEEFCAFEBABE },
+    { CityHash64WithSeed, 0xFEED0DEEDCE11BAD },
+    { CityHash64WithSeed, 0xDECAF00000DECADE },
+    { CityHash64WithSeed, 0xC0FFEEBEDBEADFAD }
+};
+
+namespace {
+    po::variables_map* ParseProgramArgs(int argc, char* argv[], po::options_description& desc)
+    {
+        auto vm = new po::variables_map;
+        po::positional_options_description p;
+        p.add("input-file", -1);
+
+        po::store(po::command_line_parser(argc, argv).options(desc).positional(p).run(), *vm);
+        po::notify(*vm);
+        return vm;
+    }
+}
+
+void populate_filter(istream & in, BloomFilter & filter) {
+    string url;
+    while (in >> url) {
+        filter.insert(url);
+    }
+}
 
 int main(int argc, char * argv[])
 {
-    using namespace std;
+    size_t opt;
+    po::options_description desc("Allowed options");
+    desc.add_options()
+        ("input-file", po::value<string>(), "input file")
+        ("size,s",   po::value<size_t>(&opt)->default_value(DEFAULT_FILTER_SIZE), "Set filter size (in bits)")
+        ("help",      "Print help message");
 
-    const vector<HashPair> defaultHashSet {
-        { CityHash64WithSeed, 0xDEADBEEFCAFEBABE },
-        { CityHash64WithSeed, 0xCAFEBABEDEADBEEF },
-        { CityHash64WithSeed, 0xDFDBEEEAFCAEBABE }
-    };
+    auto argVars = std::unique_ptr<po::variables_map>(ParseProgramArgs(argc, argv, desc));
 
-    BloomFilter<string> filter(defaultHashSet);
+    if (argVars->count("help")) {
+        cout << desc << endl;
+        return 0;
+    }
 
+    if (argVars->count("input-file") != 1) {
+        cerr << "exactly one input file is required" << endl;
+        return 1;
+    }
+
+    fstream fs;
+    fs.open((*argVars)["input-file"].as<string>(), fstream::in);
+
+    if (!fs.is_open()) {
+        cerr << "unable to open file: " << argv[1] << "\n";
+    }
+
+    size_t filterSize = (*argVars)["size"].as<size_t>();
+    BloomFilter filter(filterSize, DEFAULT_HASH_SET);
+    populate_filter(fs, filter);
     return 0;
 }
